24592ec659175c621f0a7a84af78232f9bf59630
rniedziolka <r.m.niedziolka@gmail.com>
2021-11-24 16:23:57 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  get "pages/home"
+  root :to => 'pages#home'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
