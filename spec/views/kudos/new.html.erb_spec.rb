cdcae216f5cea41c74d4325a6d1547b70375935e
rniedziolka <r.m.niedziolka@gmail.com>
2021-12-08 15:05:06 +0100

    Create new.html.erb_spec.rb

---
+++ spec/views/kudos/new.html.erb_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "kudos/new", type: :view do
+  before(:each) do
+    assign(:kudo, Kudo.new(
+      title: "MyString",
+      content: "MyText"
+    ))
+  end
+
+  it "renders new kudo form" do
+    render
+
+    assert_select "form[action=?][method=?]", kudos_path, "post" do
+
+      assert_select "input[name=?]", "kudo[title]"
+
+      assert_select "textarea[name=?]", "kudo[content]"
+    end
+  end
+end
