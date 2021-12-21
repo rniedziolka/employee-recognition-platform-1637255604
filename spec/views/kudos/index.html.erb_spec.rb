e5019c03426c7cf76412b778526c0ef052bfb004
rniedziolka <r.m.niedziolka@gmail.com>
2021-12-21 18:28:39 +0100

    Update index.html.erb_spec.rb

--- spec/views/kudos/index.html.erb_spec.rb
+++ spec/views/kudos/index.html.erb_spec.rb
@@ -1,22 +1,24 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "kudos/index", type: :view do
-  before(:each) do
+RSpec.describe 'kudos/index', type: :view do
+  before do
     assign(:kudos, [
-      Kudo.create!(
-        title: "Title",
-        content: "MyText"
-      ),
-      Kudo.create!(
-        title: "Title",
-        content: "MyText"
-      )
-    ])
+             Kudo.create!(
+               title: 'Title',
+               content: 'MyText'
+             ),
+             Kudo.create!(
+               title: 'Title',
+               content: 'MyText'
+             )
+           ])
   end
 
-  it "renders a list of kudos" do
+  it 'renders a list of kudos' do
     render
-    assert_select "tr>td", text: "Title".to_s, count: 2
-    assert_select "tr>td", text: "MyText".to_s, count: 2
+    assert_select 'tr>td', text: 'Title'.to_s, count: 2
+    assert_select 'tr>td', text: 'MyText'.to_s, count: 2
   end
 end
