1736b4ee4594e988707b6a526887dd40bfd38c81
rniedziolka <r.m.niedziolka@gmail.com>
2021-12-21 18:28:43 +0100

    Update show.html.erb_spec.rb

--- spec/views/kudos/show.html.erb_spec.rb
+++ spec/views/kudos/show.html.erb_spec.rb
@@ -1,14 +1,16 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "kudos/show", type: :view do
-  before(:each) do
+RSpec.describe 'kudos/show', type: :view do
+  before do
     @kudo = assign(:kudo, Kudo.create!(
-      title: "Title",
-      content: "MyText"
-    ))
+                            title: 'Title',
+                            content: 'MyText'
+                          ))
   end
 
-  it "renders attributes in <p>" do
+  it 'renders attributes in <p>' do
     render
     expect(rendered).to match(/Title/)
     expect(rendered).to match(/MyText/)
