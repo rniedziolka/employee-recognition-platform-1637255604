0f41a44963c16e8dfb49490094094f8c249d40d8
rniedziolka <r.m.niedziolka@gmail.com>
2021-12-21 18:28:37 +0100

    Update edit.html.erb_spec.rb

--- spec/views/kudos/edit.html.erb_spec.rb
+++ spec/views/kudos/edit.html.erb_spec.rb
@@ -1,21 +1,22 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "kudos/edit", type: :view do
-  before(:each) do
+RSpec.describe 'kudos/edit', type: :view do
+  before do
     @kudo = assign(:kudo, Kudo.create!(
-      title: "MyString",
-      content: "MyText"
-    ))
+                            title: 'MyString',
+                            content: 'MyText'
+                          ))
   end
 
-  it "renders the edit kudo form" do
+  it 'renders the edit kudo form' do
     render
 
-    assert_select "form[action=?][method=?]", kudo_path(@kudo), "post" do
+    assert_select 'form[action=?][method=?]', kudo_path(@kudo), 'post' do
+      assert_select 'input[name=?]', 'kudo[title]'
 
-      assert_select "input[name=?]", "kudo[title]"
-
-      assert_select "textarea[name=?]", "kudo[content]"
+      assert_select 'textarea[name=?]', 'kudo[content]'
     end
   end
 end
