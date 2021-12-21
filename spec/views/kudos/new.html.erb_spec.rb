d75bcbfcd8dd09b17040a0875c483864c4c0a85b
rniedziolka <r.m.niedziolka@gmail.com>
2021-12-21 18:28:41 +0100

    Update new.html.erb_spec.rb

--- spec/views/kudos/new.html.erb_spec.rb
+++ spec/views/kudos/new.html.erb_spec.rb
@@ -1,21 +1,22 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "kudos/new", type: :view do
-  before(:each) do
+RSpec.describe 'kudos/new', type: :view do
+  before do
     assign(:kudo, Kudo.new(
-      title: "MyString",
-      content: "MyText"
-    ))
+                    title: 'MyString',
+                    content: 'MyText'
+                  ))
   end
 
-  it "renders new kudo form" do
+  it 'renders new kudo form' do
     render
 
-    assert_select "form[action=?][method=?]", kudos_path, "post" do
+    assert_select 'form[action=?][method=?]', kudos_path, 'post' do
+      assert_select 'input[name=?]', 'kudo[title]'
 
-      assert_select "input[name=?]", "kudo[title]"
-
-      assert_select "textarea[name=?]", "kudo[content]"
+      assert_select 'textarea[name=?]', 'kudo[content]'
     end
   end
 end
