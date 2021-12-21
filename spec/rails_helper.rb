4f8592184b59bf6f9dfa8c192283e0f046ef1d94
rniedziolka <r.m.niedziolka@gmail.com>
2021-12-21 18:28:24 +0100

    Update rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
+require 'devise'
 
+RSpec.configure do |config|
+  config.include Devise::Test::ControllerHelpers, type: :controller
+end
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
