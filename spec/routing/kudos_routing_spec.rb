6e44f9a1da147d38e708c39074336078760fd098
rniedziolka <r.m.niedziolka@gmail.com>
2021-12-21 18:28:32 +0100

    Update kudos_routing_spec.rb

--- spec/routing/kudos_routing_spec.rb
+++ spec/routing/kudos_routing_spec.rb
@@ -1,38 +1,39 @@
-require "rails_helper"
+# frozen_string_literal: true
+
+require 'rails_helper'
 
 RSpec.describe KudosController, type: :routing do
-  describe "routing" do
-    it "routes to #index" do
-      expect(get: "/kudos").to route_to("kudos#index")
+  describe 'routing' do
+    it 'routes to #index' do
+      expect(get: '/kudos').to route_to('kudos#index')
     end
 
-    it "routes to #new" do
-      expect(get: "/kudos/new").to route_to("kudos#new")
+    it 'routes to #new' do
+      expect(get: '/kudos/new').to route_to('kudos#new')
     end
 
-    it "routes to #show" do
-      expect(get: "/kudos/1").to route_to("kudos#show", id: "1")
+    it 'routes to #show' do
+      expect(get: '/kudos/1').to route_to('kudos#show', id: '1')
     end
 
-    it "routes to #edit" do
-      expect(get: "/kudos/1/edit").to route_to("kudos#edit", id: "1")
+    it 'routes to #edit' do
+      expect(get: '/kudos/1/edit').to route_to('kudos#edit', id: '1')
     end
 
-
-    it "routes to #create" do
-      expect(post: "/kudos").to route_to("kudos#create")
+    it 'routes to #create' do
+      expect(post: '/kudos').to route_to('kudos#create')
     end
 
-    it "routes to #update via PUT" do
-      expect(put: "/kudos/1").to route_to("kudos#update", id: "1")
+    it 'routes to #update via PUT' do
+      expect(put: '/kudos/1').to route_to('kudos#update', id: '1')
     end
 
-    it "routes to #update via PATCH" do
-      expect(patch: "/kudos/1").to route_to("kudos#update", id: "1")
+    it 'routes to #update via PATCH' do
+      expect(patch: '/kudos/1').to route_to('kudos#update', id: '1')
     end
 
-    it "routes to #destroy" do
-      expect(delete: "/kudos/1").to route_to("kudos#destroy", id: "1")
+    it 'routes to #destroy' do
+      expect(delete: '/kudos/1').to route_to('kudos#destroy', id: '1')
     end
   end
 end
