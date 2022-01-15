5966bfad66c0daecfa6f7a89ddb5485ee1b0a2f8
rniedziolka <r.m.niedziolka@gmail.com>
2022-01-15 14:28:37 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get 'pages/home'
   devise_for :employees
   resources :kudos
-  root 'pages#home'
+  root 'kudos#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
