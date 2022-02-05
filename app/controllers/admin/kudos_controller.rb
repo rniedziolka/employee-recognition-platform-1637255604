6af6660faaeab360741c267bc89505551b3c7f49
rniedziolka <r.m.niedziolka@gmail.com>
2022-02-05 14:49:48 +0100

    Implement_CRUD_actions_for_Kudo_FIXES

--- app/controllers/admin/kudos_controller.rb
+++ app/controllers/admin/kudos_controller.rb
@@ -3,36 +3,29 @@
 module Admin
   class KudosController < AdminController
     before_action :set_kudo, only: %i[show edit update destroy]
-    before_action :authenticate_admin_user!
+    #  before_action :authenticate_admin_user!
 
-    # GET /admin/kudos
     def index
-      @kudos = Kudo.all
+      @kudos = Kudo.all.includes(:employee, :receiver)
     end
 
-    # GET /admin/kudos/1
     def show; end
 
-    # GET /admin/kudos/new
     def new
       @kudo = Kudo.new
     end
 
-    # GET /admin/kudos/1/edit
     def edit; end
-
-    # POST /admin/kudos
 
     def create
       @kudo = Kudo.new(kudo_params)
+
       if @kudo.save
         redirect_to admin_kudos_path(@kudo), notice: 'Kudo was successfully created.'
       else
         render :new
       end
     end
-
-    # PATCH/PUT /admin/kudos/1
 
     def update
       if @kudo.update(kudo_params)
@@ -42,7 +35,6 @@ module Admin
       end
     end
 
-    # DELETE /admin/kudos/1
     def destroy
       @kudo.destroy
       redirect_to admin_kudos_url, notice: 'Kudo was successfully destroyed.'
@@ -50,14 +42,12 @@ module Admin
 
     private
 
-    # Use callbacks to share common setup or constraints between actions.
     def set_kudo
       @kudo = Kudo.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def kudo_params
-      params.require(:kudo).permit(:title, :content, :employee_id)
+      params.require(:kudo).permit(:title, :content, :employee_id, :receiver_id)
     end
   end
 end
