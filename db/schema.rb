d4edd28cb7dc3a114a76560c602f12d1978eefe7
rniedziolka <r.m.niedziolka@gmail.com>
2022-02-17 10:31:25 +0100

    bugfix/fixed-company-value-id

--- db/schema.rb
+++ db/schema.rb
@@ -52,7 +52,7 @@ ActiveRecord::Schema.define(version: 2022_02_14_114403) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "employee_id"
     t.integer "receiver_id"
-    t.bigint "company_value_id"
+    t.bigint "company_value_id", null: false
     t.index ["company_value_id"], name: "index_kudos_on_company_value_id"
     t.index ["employee_id"], name: "index_kudos_on_employee_id"
     t.index ["receiver_id"], name: "index_kudos_on_receiver_id"
@@ -64,18 +64,6 @@ ActiveRecord::Schema.define(version: 2022_02_14_114403) do
     t.decimal "price", precision: 8, scale: 2, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-  end
-    
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "kudos", "company_values"
