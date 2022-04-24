a33bc37c93229a23cad7e139666618b399f04ffc
rniedziolka <r.m.niedziolka@gmail.com>
2022-04-24 17:09:38 +0200

    Revert "hotfix/update-environment-settings-3"
    This reverts commit 8df903c8c9fbba7876faf1f74e56a68302849a38.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,17 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+
+  ActionMailer::Base.smtp_settings = {
+    user_name: 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
+    password: Rails.application.credentials.dig(:sendgrid, :api_key), #   This is the secret sendgrid API key which was issued during API key creation
+    domain: 'employee-platfrom.herokuapp.com',
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
