20202eb9639be71c1ba52fca31a2c8b341a6b34d
rniedziolka <r.m.niedziolka@gmail.com>
2022-05-11 10:02:07 +0200

    bugfix/allowing-removal-rewards-despite-order

--- db/migrate/20220222114752_create_orders.rb
+++ db/migrate/20220222114752_create_orders.rb
@@ -2,7 +2,7 @@ class CreateOrders < ActiveRecord::Migration[6.1]
   def change
     create_table :orders do |t|
       t.references :employee, null: false, foreign_key: true
-      t.references :reward, null: false, foreign_key: true
+      t.references :reward, foreign_key: true
 
       t.timestamps
     end
