3aff0df35a8eae15537081d6cc0039f61422ed52
rniedziolka <r.m.niedziolka@gmail.com>
2022-07-20 14:06:31 +0200

    bugfix/eliminating-N+1-issue

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -4,7 +4,7 @@ module Admin
   class OrdersController < AdminController
     def index
       respond_to do |format|
-        format.html { render :index, locals: { orders: Order.includes(:employee).all.order(:status) } }
+        format.html { render :index, locals: { orders: Order.includes(:employee, :reward, employee: :addresses).all.order(:status) } }
         format.csv { send_data Export::OrderExport.new(Order.includes(:employee).all.order(:status)).to_csv, filename: "Orders_#{Time.zone.today}.csv" }
       end
     end
